1662ca5ae9d97d983b5fe7728bb78860e911ff74
unknown <alison.messex@outlook.com>
2021-09-27 19:28:06 -0400

    first commit

---
+++ weather.py
@@ -0,0 +1,14 @@
+temperature = 75
+forecast = "rain"
+
+if temperature > 80 or temperature < 60:
+    print("Stay inside!")
+else:
+    print("Enjoy the outdoors!")
+
+if not forecast == "rain":
+    print("Go outside!")
+else:
+    print("Stay inside!")
+
+print("Have a good day!")
